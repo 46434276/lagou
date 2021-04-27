24d451bc3c0edd00af7f01453a17d8337e28b822
老孙 <46434276@qq.com>
2021-04-27 14:27:38 +0800

    初始化

---
+++ src/main/java/test1/A.java
@@ -0,0 +1,13 @@
+package test1;
+
+/**
+ * @BelongsProject: laosun
+ * @Author: GuoAn.Sun
+ * @CreateTime: 2021-04-27 14:24
+ * @Description:
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
